df713d7df735e8c7c44ea59ed08094a594745855
Afzal Najam <afzalnaj@gmail.com>
2021-12-12 23:32:27 -0500

    More cleanup

--- app/src/main/java/com/afzaln/materialcontainertransformdemo/MainActivity.kt
+++ app/src/main/java/com/afzaln/materialcontainertransformdemo/MainActivity.kt
@@ -10,7 +10,6 @@ import com.afzaln.materialcontainertransformdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +20,5 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-            || super.onSupportNavigateUp()
     }
 }
